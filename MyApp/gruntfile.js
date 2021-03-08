429388f5f06057e73236d43a77b3348fdd185d33
Keith Ecker <keith.ecker@flexagon.com>
2021-03-08 14:44:54 -0600

    Added app

---
+++ MyApp/gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        //Package containing our Node modules
+        pkg: grunt.file.readJSON('package.json'),
+        
+        //Tasks
+        copy: {
+            //Targets/Context
+            t1: {
+                src: 'dir1/**',
+                dest: 'dir2/'
+            }
+        } 
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+};
